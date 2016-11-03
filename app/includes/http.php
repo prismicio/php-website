aa0c7497b5a14afbb9ac0b3420d612754860d553
prismic-levi <lga@zengularity.com>
2016-11-03 14:34:17 +0100

    Fix firstrun

--- app/includes/http.php
+++ app/includes/http.php
@@ -21,13 +21,12 @@ function render($app, $page, $data = array())
   }
 }
 
-function not_found($app, $skin = null)
+function not_found($app)
 {
   $file_path = views_dir().'/404.php';
   if (file_exists($file_path)) { // Avoid an infinite loop
    render($app, '404');
   } else {
    echo '<h1>404 Not found</h1>';
-   echo 'Additionnaly the 404 template seems to be missing from the skin.';
   }
 }
